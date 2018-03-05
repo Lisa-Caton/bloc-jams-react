b7f1e31c5edcf52aea6c1be29df2e6d438f6e0d1
Lisa Caton <lisacaton01@gmail.com>
2018-03-05 10:28:58 -0500

    Empty component - update

---
+++ src/components/Album.js
@@ -0,0 +1,11 @@
+class Album extends Component {
+  render() {
+    return (
+      <div className="Album">
+      <p>Lorem ipsum dolor sit amet</p>
+      </div>
+    );
+  }
+}
+
+export default Album;
