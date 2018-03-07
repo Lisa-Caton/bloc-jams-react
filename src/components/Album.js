6c27540e70010bbcf794b33c3c7e79bfe5bdf75c
Lisa Caton <lisacaton01@gmail.com>
2018-03-06 19:38:39 -0500

    Components: Album // working properly

--- src/components/Album.js
+++ src/components/Album.js
@@ -6,11 +6,13 @@ class Album extends Component {
     super(props);
 
     const album = albumData.find( album => {
+      console.log("Is " + album.slug + " the same as " + this.props.match.params.slug + "?");
       return album.slug === this.props.match.params.slug
     });
+    debugger;
 
     this.state = {
-      album: album
+      album: albumData
     };
   }
 
