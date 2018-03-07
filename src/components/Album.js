f57e5268bb08995d8d96efa069403cfba14d838a
Lisa Caton <lisacaton01@gmail.com>
2018-03-07 12:59:26 -0500

    table for song list created

--- src/components/Album.js
+++ src/components/Album.js
@@ -25,6 +25,15 @@ class Album extends Component {
             <h2 className="artist">{this.state.album.artist}</h2>
             <div id="release-info">{this.state.album.releaseInfo}</div>
           </div>
+          <table id="song-list">
+            <colgroup>
+              <col id="song-number-column" />
+              <col id="song-title-column" />
+              <col id="song-duration-column" />
+            </colgroup>
+            <tbody>
+            </tbody>
+          </table>
         </section>
       </section>
     );
